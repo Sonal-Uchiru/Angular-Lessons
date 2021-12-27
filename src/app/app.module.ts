465d1733ba117e2955eebc2da7206eba73dd9297
sonal-uchiru <sonal123326@gmail.com>
2021-12-27 18:40:50 +0530

    session 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
+import { MainPageComponent } from './component/main-page/main-page.component';
+import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MainPageComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  title: string = "My Home"
+  number: number = 0
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  toggleIncrement() {
+    // @ts-ignore
+    this.number += 1
+  }
+
+
+  toggleDecrement() {
+    this.number -= 1
+  }
 }
